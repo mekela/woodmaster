e8af4f385529fdaf0ebd159b2a72a64f608d98f0
alex <sania_free@i.ua>
2016-07-25 18:15:40 +0300

    add 1 page

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//bxslider
+	$('.reviews_slider ul').bxSlider();
+});
