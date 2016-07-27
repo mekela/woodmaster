41cf257e71697964af4a3ce47776a529a23bda72
alex <sania_free@i.ua>
2016-07-27 17:00:30 +0300

    add scripts

--- js/common.js
+++ js/common.js
@@ -13,4 +13,29 @@ $(document).ready(function() {
 
 	//tel
 	$("[type='tel'").mask("+7 (999) 999-9999");
+
+	//show menu
+	$( ".logo_drop" ).click(function() {
+	  $( ".logo_menu" ).slideToggle( "slow");
+	});
+	$( ".menu_trigger i" ).click(function() {
+	  $( ".head_menu ul" ).slideToggle( "slow");
+	});
+
+	 //scroll anchor
+    $('.totop').on('click',function(){
+		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top-1},800);
+		return false;
+	});
+});
+
+//fixed header
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 600) {
+        $(".header").addClass("fixed_element");
+    } else {
+        $(".header").removeClass("fixed_element");
+    }
 });
